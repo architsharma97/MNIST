8c30a071425491910f7ac936d4cb7493a9a27298
architsharma97 <archit97sharma@gmail.com>
2017-06-06 16:10:30 +0530

    Reformulatin SF estimators

--- main.py
+++ main.py
@@ -293,7 +293,7 @@ if args.mode == 0:
 		elif args.latent_type =='disc':
 			# for stability of gradients
 			latent_probs_clipped = T.clip(latent_probs, 1e-7, 1-1e-7)
-			cost_encoder = T.mean(reconstruction_loss * -T.nnet.nnet.binary_crossentropy(latent_probs_clipped, latent_samples).sum(axis=1))
+			cost_encoder = T.mean((reconstruction_loss - T.mean(reconstruction_loss)) * -T.nnet.nnet.binary_crossentropy(latent_probs_clipped, latent_samples).sum(axis=1))
 
 		# regularization
 		weights_sum_enc = 0.
