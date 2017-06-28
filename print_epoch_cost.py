d2c4bbadef13d5607ffa9aec8ecfddaa86f9e452
architsharma97 <archit97sharma@gmail.com>
2017-06-28 11:42:01 -0400

    Updated print epoch cost

--- print_epoch_cost.py
+++ print_epoch_cost.py
@@ -2,7 +2,12 @@ import sys
 
 f = open(sys.argv[1],'r').read().splitlines()
 
-for i in range(1000):
+if len(sys.argv) > 2:
+	r = int(sys.arg[2])
+else:
+	r = 1000
+
+for i in range(r):
 	sum = 0.0
 	for j in range(600):
 		sum += float(f[i*600 + j].split(',')[2])
