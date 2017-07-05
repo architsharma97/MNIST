a259ba4ba0f49762399062657d1de3bb1c00b855
architsharma97 <archit97sharma@gmail.com>
2017-07-04 22:39:26 -0400

    Added bias-variance decompostion for straight through and REINFORCE

--- gradcomp.py
+++ gradcomp.py
@@ -14,20 +14,15 @@ import time
 
 
 '''
-Compare the gradients produced by ST estimator and REINFORCE.
+Compares different gradients in terms of bias and variance. Generates 250 latent samples per example with 250-sample REINFORCE considered as the true gradient.
 '''
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 parser = argparse.ArgumentParser()
 # training configuration
-parser.add_argument('-m', '--mode', type=str, default='train', 
-					help='train or test, note you still have to set the configure the latent type and estimator for proper testing.')
-parser.add_argument('-x', '--estimator', type=str, default='ST',
-					help='Primary estimator to train the network: Either straight through (ST) or REINFORCE (SF)')
-parser.add_argument('-r', '--repeat', type=int, default=1, 
+parser.add_argument('-r', '--repeat', type=int, default=250, 
 					help='Determines the number of samples per training example for SF estimator')
-parser.add_argument('-o', '--latent_type', type=str, default='disc', 
-					help='No other option')
+
 # hyperparameters
 parser.add_argument('-a', '--learning_rate', type=float, default=0.0001, help='Learning rate')
 parser.add_argument('-b', '--batch_size', type=int, default=100, help='Size of the minibatch used for training')
@@ -43,12 +38,6 @@ parser.add_argument('-d', '--min_cost', type=float, default=55.0,
 # while testing
 parser.add_argument('-l', '--load', type=str, default=None, help='Path to weights')
 
-# saving
-parser.add_argument('-s', '--save_freq', type=int, default=100, 
-					help='Number of epochs after which weights should be saved')
-parser.add_argument('-f', '--base_code', type=str, default='',
-					help='A unique identifier for saving purposes')
-
 # miscellaneous
 parser.add_argument('-q', '--random_seed', type=int, default=42, help='Seed to initialize random streams')
 
@@ -60,7 +49,7 @@ if "gpu" in theano.config.device:
 else:
 	srng = T.shared_randomstreams.RandomStreams(seed=args.random_seed)
 
-code_name = args.base_code + '_' + str(args.repeat)
+code_name = str(args.repeat)
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # converts images into binary images for simplicity
@@ -146,45 +135,28 @@ for key, val in params.iteritems():
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Training Graph
-if args.mode == 'train':
-	print "Constructing graph for training"
-	# create shared variables for dataset for easier access
-	top = np.asarray([splitimg[0] for splitimg in trp], dtype=np.float32)
-	bot = np.asarray([splitimg[1] for splitimg in trp], dtype=np.float32)
-	
-	train = theano.shared(top, name='train')
-	train_gt = theano.shared(bot, name='train_gt')
+print "Constructing graph for training"
+# create shared variables for dataset for easier access
+top = np.asarray([splitimg[0] for splitimg in trp], dtype=np.float32)
+bot = np.asarray([splitimg[1] for splitimg in trp], dtype=np.float32)
 
-	# pass a batch of indices while training
-	img_ids = T.vector('ids', dtype='int64')
-	img = train[img_ids, :]
-	gt = train_gt[img_ids, :]
+train = theano.shared(top, name='train')
+train_gt = theano.shared(bot, name='train_gt')
 
-	gt = T.extra_ops.repeat(gt, args.repeat, axis=0)
+# pass a batch of indices while training
+img_ids = T.vector('ids', dtype='int64')
+img = train[img_ids, :]
+gt = train_gt[img_ids, :]
 
-# Test graph
-else:
-	print "Constructing the test graph"
-	# create shared variables for dataset for easier access
-	top = np.asarray([splitimg[0] for splitimg in tep], dtype=np.float32)
-	bot = np.asarray([splitimg[1] for splitimg in tep], dtype=np.float32)
-	
-	test = theano.shared(top, name='train')
-	test_gt = theano.shared(bot, name='train_gt')
-
-	# image ids
-	img_ids = T.vector('ids', dtype='int64')
-	img = test[img_ids,:]
-	gt = test_gt[img_ids,:]
-
+gt = T.extra_ops.repeat(gt, args.repeat, axis=0)
 
 # encoding
 out1 = fflayer(tparams, img, _concat(ff_e, 'i'))
 out2 = fflayer(tparams, out1, _concat(ff_e,'h'))
 
 latent_probs = fflayer(tparams, out2, _concat(ff_e, 'bern'), nonlin='sigmoid')
-latent_probs = T.extra_ops.repeat(latent_probs, args.repeat, axis=0)
-latent_probs_clipped = T.clip(latent_probs, 1e-7, 1-1e-7)
+latent_probs_r = T.extra_ops.repeat(latent_probs, args.repeat, axis=0)
+latent_probs_clipped = T.clip(latent_probs_r, 1e-7, 1-1e-7)
 
 # sample a bernoulli distribution, which a binomial of 1 iteration
 latent_samples_uncorrected = srng.binomial(size=latent_probs_clipped.shape, n=1, p=latent_probs_clipped, dtype=theano.config.floatX)
@@ -199,139 +171,90 @@ outh = fflayer(tparams, outz, _concat(ff_d, 'h'))
 probs = fflayer(tparams, outh, _concat(ff_d, 'o'), nonlin='sigmoid')
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# Training
-if args.mode == 'train':
+# having large number of latent samples is necessary: multi-sample REINFORCE essentially gives the true gradients at the cost of speed.
+reconstruction_loss = T.nnet.binary_crossentropy(probs, gt).sum(axis=1)
 
-	reconstruction_loss = T.nnet.binary_crossentropy(probs, gt).sum(axis=1)
+# separate parameters for encoder and decoder
+param_dec = [val for key, val in tparams.iteritems() if 'ff_dec' in key]
+param_enc = [val for key, val in tparams.iteritems() if 'ff_enc' in key]
 
-	# separate parameters for encoder and decoder
-	param_dec = [val for key, val in tparams.iteritems() if 'ff_dec' in key]
-	param_enc = [val for key, val in tparams.iteritems() if 'ff_enc' in key]
-	
-	# decoder gradients
-	cost_decoder = T.mean(reconstruction_loss)
-	grads_decoder = T.grad(cost_decoder, wrt=param_dec)
+# decoder gradients
+cost_decoder = T.mean(reconstruction_loss)
+grads_decoder = T.grad(cost_decoder, wrt=param_dec)
 
-	# REINFORCE gradients: conditional mean is subtracted from the reconstruction loss to lower variance further
-	baseline = T.extra_ops.repeat(fflayer(tparams, T.concatenate([img, train_gt[img_ids, :]], axis=1), 'loss_pred', nonlin='relu'), args.repeat, axis=0)
-	cost_encoder = T.mean((reconstruction_loss - baseline.T) * -T.nnet.nnet.binary_crossentropy(latent_probs_clipped, latent_samples).sum(axis=1))
-	consider_constant = [reconstruction_loss, latent_samples, baseline]
-	grads_encoder_reinforce = T.grad(cost_encoder, wrt=param_enc, consider_constant=consider_constant)
-	
-	# optimizing the loss predictor for conditional mean baseline
-	cost_pred = 0.5 * ((reconstruction_loss - baseline.T) ** 2).sum()
-	params_loss_predictor = [val for key, val in tparams.iteritems() if 'loss_pred' in key]
-	grads_plp = T.grad(cost_pred, wrt=params_loss_predictor, consider_constant=[reconstruction_loss])
+# REINFORCE gradients: conditional mean is subtracted from the reconstruction loss to lower variance further
+baseline = T.extra_ops.repeat(fflayer(tparams, T.concatenate([img, train_gt[img_ids, :]], axis=1), 'loss_pred', nonlin='relu'), args.repeat, axis=0)
+cost_encoder = T.mean((reconstruction_loss - baseline.T) * -T.nnet.nnet.binary_crossentropy(latent_probs_clipped, latent_samples).sum(axis=1))
+consider_constant = [reconstruction_loss, latent_samples, baseline]
+grads_encoder = T.grad(cost_encoder, wrt=param_enc + [latent_probs_r] + [latent_probs], consider_constant=consider_constant)
 
-	# ST gradients: uses the same cost as decoder, with identity gradient flow through bernoulli sampling
-	grads_encoder_st = T.grad(cost_decoder, wrt=param_enc, consider_constant=[dummy])
+# true gradient is scaled down 100 times example wise and 1-sample reinforce is scaled down by 250*100, otherwise it is correct
+true_gradient = args.batch_size * grads_encoder[-1]
+reinforce_1 = args.batch_size * args.repeat * grads_encoder[-2]
+grads_encoder = grads_encoder[:-2]
 
-	# grads to be compared
-	gradlp_reinforce = T.grad(cost_encoder, wrt=latent_probs_clipped, consider_constant=consider_constant)
-	gradlp_st = T.grad(cost_decoder, wrt=latent_probs_clipped, consider_constant=[dummy])
-	diff = ((gradlp_st - gradlp_reinforce)**2).sum()
-	mean_pos_cos = T.mean(((gradlp_st * gradlp_reinforce).sum(axis=1)) > 0)
-	
-	if args.estimator == 'SF':
-		grads_encoder = grads_encoder_reinforce
-	else:
-		grads_encoder = grads_encoder_st
+# optimizing the loss predictor for conditional mean baseline
+cost_pred = 0.5 * ((reconstruction_loss - baseline.T) ** 2).sum()
+params_loss_predictor = [val for key, val in tparams.iteritems() if 'loss_pred' in key]
+grads_plp = T.grad(cost_pred, wrt=params_loss_predictor, consider_constant=[reconstruction_loss])
 
-	grads = grads_encoder + grads_plp + grads_decoder
+grads = grads_encoder + grads_plp + grads_decoder
 
-	# learning rate
-	lr = T.scalar('lr', dtype='float32')
+# computation of different gradients, bias and variances: we have already computed true gradient example wise above
+temp = T.extra_ops.repeat(true_gradient, args.repeat, axis=0)
 
-	inps = [img_ids]
-	outs = [cost_decoder, diff, mean_pos_cos]
+# bias-variance of 1-sample reinforce: expected value for the gradient is the true gradient itself: bias should be zero
+bias2_reinforce = ((reinforce_1.reshape((args.batch_size, args.repeat, latent_dim)).sum(axis=1) / args.repeat - true_gradient) ** 2).sum() / args.batch_size
+var_reinforce = ((reinforce_1 - temp) ** 2).sum() / (args.repeat * args.batch_size)
 
-	print "Setting up optimizer"
-	f_grad_shared, f_update = adam(lr, tparams, grads, inps, outs)
+# computation of straight-through estimator example wise.
+st = args.batch_size * args.repeat * T.grad(cost_decoder, wrt=latent_probs_r, consider_constant=[dummy])
+ez_st = st.reshape((args.batch_size, args.repeat, latent_dim)).sum(axis=1) / args.repeat
+bias2_st = ((ez_st - true_gradient) ** 2).sum() / args.batch_size
+var_st = ((st - T.extra_ops.repeat(ez_st, args.repeat, axis=0)) ** 2).sum() / (args.repeat * args.batch_size)
 
-	print "Training"
-	cost_report = open('./Results/' + args.latent_type + '/' + args.estimator + '/gradcomp_' + code_name + '_' + str(args.batch_size) + '_' + str(args.learning_rate) + '.txt', 'w')
-	id_order = range(len(trc))
+# learning rate
+lr = T.scalar('lr', dtype='float32')
 
-	iters = 0
-	min_cost = 100000.0
-	epoch = 0
-	condition = False
+inps = [img_ids]
+outs = [cost_decoder, bias2_reinforce, var_reinforce, bias2_st, var_st]
 
-	while condition == False:
-		print "Epoch " + str(epoch + 1),
+print "Setting up optimizer"
+f_grad_shared, f_update = adam(lr, tparams, grads, inps, outs)
 
-		np.random.shuffle(id_order)
-		epoch_cost = 0.
-		epoch_diff = 0.
-		epoch_start = time.time()
-		for batch_id in range(len(trc)/args.batch_size):
-			batch_start = time.time()
+print "Training"
+cost_report = open('./Results/disc/SF/gradcomp_' + code_name + '_' + str(args.batch_size) + '_' + str(args.learning_rate) + '.txt', 'w')
+id_order = range(len(trc))
 
-			idlist = id_order[batch_id*args.batch_size:(batch_id+1)*args.batch_size]
-			cost, diffc, mpc = f_grad_shared(idlist)	
-			min_cost = min(min_cost, cost)
+iters = 0
+min_cost = 100000.0
+epoch = 0
+condition = False
 
-			f_update(args.learning_rate)
+while condition == False:
+	print "Epoch " + str(epoch + 1),
 
-			epoch_cost += cost
-			epoch_diff += diffc
-			cost_report.write(str(epoch) + ',' + str(batch_id) + ',' + str(cost) + ',' + str(diffc) + ',' + str(mpc) + ',' + str(time.time() - batch_start) + '\n')
+	np.random.shuffle(id_order)
+	epoch_cost = 0.
+	epoch_diff = 0.
+	epoch_start = time.time()
+	for batch_id in range(len(trc)/args.batch_size):
+		batch_start = time.time()
 
-		print ": Cost " + str(epoch_cost) + " : Difference " + str(epoch_diff) + " : Time " + str(time.time() - epoch_start)
+		idlist = id_order[batch_id*args.batch_size:(batch_id+1)*args.batch_size]
 		
-		# save every args.save_freq epochs
-		if (epoch + 1) % args.save_freq == 0:
-			print "Saving..."
+		cost, br, vr, bs, vs = f_grad_shared(idlist)	
+		min_cost = min(min_cost, cost)
+		f_update(args.learning_rate)
 
-			params = {}
-			for key, val in tparams.iteritems():
-				params[key] = val.get_value()
+		epoch_cost += cost
+		# epoch, batch id, bias-reinforce, variance-reinforce, bias-straight through, variance-straigt through, time of computation
+		cost_report.write(str(epoch) + ',' + str(batch_id) + ',' + str(cost)  + ',' + str(br) + ',' + str(vr) + ',' + str(bs) + ',' + str(vs) + ',' + str(time.time() - batch_start) + '\n')
 
-			# numpy saving
-			np.savez('./Results/' + args.latent_type + '/' + args.estimator + '/training_' + code_name + '_' + str(args.batch_size) + '_' + str(args.learning_rate) + '_' + str(epoch+1) + '.npz', **params)
-			print "Done!"
+	print ": Cost " + str(epoch_cost) + " : Time " + str(time.time() - epoch_start)
 
-		epoch += 1
-		if args.term_condition == 'mincost' and min_cost < args.min_cost:
-			condition = True
-		elif args.term_condition == 'epochs' and epoch >= args.num_epochs:
-			condition = True
-	
-	# saving the final model
-	if epoch % args.save_freq != 0:
-		print "Saving..."
-
-		params = {}
-		for key, val in tparams.iteritems():
-			params[key] = val.get_value()
-
-		# numpy saving
-		np.savez('./Results/' + args.latent_type + '/' + args.estimator + '/training_' + code_name + '_' + str(args.batch_size) + '_' + str(args.learning_rate) + '_' + str(epoch) + '.npz', **params)
-		print "Done!"
-
-# Test
-else:
-	# useful for one example at a time only
-	prediction = probs > 0.5
-
-	loss = abs(prediction - gt).sum()
-
-	# compiling test function
-	inps = [img_ids]
-	if args.estimator == 'PD' and args.latent_type =='disc':
-		inps += [temperature]
-
-	f = theano.function(inps, [prediction, loss])
-	idx = 10
-	if args.estimator == 'PD' and args.latent_type =='disc':
-		pred, loss = f([idx], 0.5)
-	else:
-		pred, loss = f([idx])
-
-	show(tec[idx].reshape(28,28))
-
-	reconstructed_img = np.zeros((28*28,))
-	reconstructed_img[:14*28] = tep[idx][0]
-	reconstructed_img[14*28:] = pred
-	show(reconstructed_img.reshape(28,28))
-	print loss
+	epoch += 1
+	if args.term_condition == 'mincost' and min_cost < args.min_cost:
+		condition = True
+	elif args.term_condition == 'epochs' and epoch >= args.num_epochs:
+		condition = True
