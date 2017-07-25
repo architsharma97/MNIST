0b8ff22cd3084acd315d0968396c2cdf1af41054
architsharma97 <archit97sharma@gmail.com>
2017-07-25 02:07:49 -0400

    Learning schedule

--- stochasticdni.py
+++ stochasticdni.py
@@ -567,10 +567,11 @@ if args.mode == 'train':
 
 	while condition == False:
 		# handcrafted learning rate schedule: Every 20 epochs slash learning rate by half
-		# if iters % (20 * 600) == 0 and args.sg_learning_rate > 1e-6:
-		# 	print "Updated subnetwork learning rate"
-		# 	args.sg_learning_rate /= 2
-		# 	sgd.lr.set_value(args.sg_learning_rate)
+		if iters % (20 * 600) == 0 and args.sg_learning_rate > 1e-6 and args.learning_rate > 1e-6:
+			print "Updated subnetwork learning rate"
+			args.sg_learning_rate /= 2
+			args.learning_rate /= 2
+			sgd.lr.set_value(args.sg_learning_rate)
 			
 		print "Epoch " + str(epoch + 1),
 		np.random.shuffle(id_order)
